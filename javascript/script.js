fa7579f2f8cbe38309f774bcd5e7c3967f344a6b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-02 16:34:45 +0100

    Finished about me page

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+﻿const button = document.querySelector("#tech-stack-button");
+const techStack = document.querySelector("#tech-stack");
+const anchor = document.querySelector("#tech-stack-anchor")
+
+
+anchor.addEventListener("click", (event) => {
+    event.preventDefault()
+})
+button.addEventListener("click", () => {
+    button.classList.add("remove-tech-stack-button");
+    techStack.classList.add("display-tech-stack");
+});
+
+// button.style.display = "none"; // Hide the button
+// techStack.style.visibility = "visible"; // Show the div with the icons
